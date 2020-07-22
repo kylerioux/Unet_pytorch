781427b0010c923eda1ad5131f993ed29b1fd112
krioux5 <krioux5@uwo.ca>
2020-07-21 21:35:56 -0400

    adding some code to convert to csv so I can make a dataframe for my network

--- convertToCSV.py
+++ convertToCSV.py
@@ -2,17 +2,17 @@
 import os
 import pandas as pd
 
-BASE_DIR = 'images/'
+BASE_DIR = 'images/processed_images/'
 train_folder = BASE_DIR+'train/'
-train_annotation = BASE_DIR+'annotated_train_data/'
+train_annotation = BASE_DIR+'train_masks/'
 
 files_in_train = sorted(os.listdir(train_folder))
 files_in_annotated = sorted(os.listdir(train_annotation))
 
-images=[i for i in files_in_train if i in files_in_annotated]
+images = [i for i in files_in_train if i in files_in_annotated]
 
 df = pd.DataFrame()
-df['images']=[train_folder+str(x) for x in images]
-df['labels']=[train_annotation+str(x) for x in images]
+df['images'] = [train_folder+str(x) for x in images]
+df['labels'] = [train_annotation+str(x) for x in images]
 
-pd.to_csv('files_path.csv', header=None)
+df.to_csv('files_path.csv', header=None)
