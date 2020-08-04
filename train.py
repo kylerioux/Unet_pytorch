13a5c4d109cd63e5b28eaec6cd7bd654ab436f19
kylerioux <krioux5@uwo.ca>
2020-08-04 16:46:07 -0400

    training works here! loss calculated correctly, changed outputs in U-net

--- train.py
+++ train.py
@@ -1,4 +1,4 @@
-# this python file contains code to train the U-net
+#python file contains code to train the U-net
 # modifying code from https://medium.com/analytics-vidhya/pytorch-implementation-of-semantic-segmentation-for-single-class-from-scratch-81f96643c98c
 
 # visualization library
@@ -20,7 +20,7 @@ from sklearn.model_selection import train_test_split
 import segmentation_models_pytorch as smp #-- this was to get u-net architecture
 # augmenation library
 from albumentations import (HorizontalFlip, ShiftScaleRotate, Normalize, Resize, Compose, GaussNoise)
-from albumentations.pytorch import ToTensor
+from albumentations.pytorch import (ToTensor, ToTensorV2)
 # others
 import os
 import pdb
@@ -34,64 +34,118 @@ warnings.filterwarnings("ignore")
 
 from UNet import UNet # get the U-net model 
 
-# image directories
-#train_img_dir = 'images/processed_images/train'
-#train_img_masks_dir = 'images/processed_images/train_masks'
+#loss.requres_grad = True ##RuntimeError: element 0 of tensors does not require grad and does not have a grad_fn
 
 #kaggle csv
-df=pd.read_csv('kaggle_data/train_masks_kaggle.csv')
+# df=pd.read_csv('kaggle_data/train_masks_kaggle.csv')
 
 # kaggle locations of images
-train_img_dir='kaggle_data/carvana_my_unet/proc/train-572-grayscale'
-train_img_masks_dir='kaggle_data/carvana_my_unet/proc/train_masks-388'
+# train_img_dir='kaggle_data/train-128'
+# train_img_masks_dir='kaggle_data/train_masks-128'
 
 
+
+# image directories
+train_img_dir = 'images/processed_images/train'
+train_img_masks_dir = 'images/processed_images/train_masks'
 
 #read csv file of image names
-#df=pd.read_csv('image_names.csv')
+df=pd.read_csv('image_names.csv')
 
 #mean, std = (0.485, 0.456, 0.406),(0.229, 0.224, 0.225) # for rbg images, related to imagenet
-mean, std = 0,255 # for grayscale images
+mean = 0
+std = 255
+
+torch.set_printoptions(threshold=50000) # for debugging, allows to print more of tensors
 
 # during traning eval phase make a list of transforms to be used.
 # inputs "phase", mean, std
 # outputs list of transformations
 def get_transform(phase,mean,std):
     list_trans=[]
-    if phase=='train':
+    if phase == 'train':
         list_trans.extend([HorizontalFlip(p=0.5)])
     list_trans.extend([Normalize(mean=mean,std=std,p=1),ToTensor()])  #normalizing the data & then converting to tensors
-    list_trans=Compose(list_trans)
+    list_trans = Compose(list_trans)
+    #print("trans list" )
+    #print(list_trans)
     return list_trans
+
+from PIL import Image
+import torchvision.transforms.functional as TF
+isTraining=0
 
 # when dataloader requests samples using index it fetches input image and target mask,
 # applys transformation and returns it
 class CityDataset(Dataset):
     def __init__(self, df, train_img_dir, train_img_masks_dir, mean, std, phase):
-        #self.fname = df['images'].values.tolist()
-        self.fname = df['img'].values.tolist() #kaggle one
+        self.fname = df['images'].values.tolist()
+        #self.fname = df['img'].values.tolist() #kaggle one - their csv has this column name
         self.train_img_dir = train_img_dir
         self.train_img_masks_dir = train_img_masks_dir
         self.mean = mean
         self.std = std
         self.phase = phase
-        self.transform = get_transform(phase,mean,std)
-    def __getitem__(self, idx):
-        name = self.fname[idx] # this is the [img] colmn in the csv file
-        
-        img_name_path = os.path.join(self.train_img_dir,name)
-        mask_name_path = img_name_path.split('.')[0].replace('train-572-grayscale','train_masks-388')+'_mask_mask.png' #kaggle dirs
-        #mask_name_path = os.path.join(self.train_img_masks_dir,name)
+        self.isTraining=isTraining
+        #self.transform = get_transform(phase,mean,std)
 
+    def transform(self,image,mask,isTraining):
+        #flip image if training and at 50% chance
+        if random.random ()>0.5 and isTraining==1.:
+            image = TF.hflip(image)       
+            mask = TF.hflip(mask)
+
+        #torch.set_printoptions(profile="full") 
+        #convert to tensor
+        image = TF.to_tensor(image)
+        #print(type(mask))
+        #print("mask before tensor conv: ")
+        #pixels=list(mask.getdata())
+        #print(pixels)
+        mask = TF.to_tensor(mask)
+        mask=mask*256
+        mask=(mask*10**0).round()/(10**0)
+        #print("mask after tensor conv: ")
+        #print(mask)
+        #torch.set_printoptions(profile="default") 
+
+
+
+        image = TF.normalize(image, mean=mean, std=std)
+        
+        return image, mask
+
+    def __getitem__(self, idx):
+        name = self.fname[idx]
+        img_name_path = os.path.join(self.train_img_dir,name)
+        #mask_name_path=img_name_path.split('.')[0].replace('train-128','train_masks-128')+'_mask.png' #kaggle dirs
+        mask_name_path = os.path.join(self.train_img_masks_dir,name)
 
         img = cv2.imread(img_name_path, cv2.IMREAD_GRAYSCALE) #added to make this grayscale similar to below line
         #img = cv2.imread(img_name_path) #non grayscale (rgb) version 
         mask = cv2.imread(mask_name_path, cv2.IMREAD_GRAYSCALE)
-        
+        #print("mask size: ")
+        #print(mask.shape)
+        #augmentation = self.transform(image=img,mask=mask)
 
-        augmentation = self.transform(image=img, mask=mask)
-        img_aug = augmentation['image'] #[1,572,572] type:Tensor
-        mask_aug = augmentation['mask'] #[1,572,572] type:Tensor
+        #img_aug = augmentation['image'] #[1,572,572] type:Tensor
+        #mask_aug = augmentation['mask'] #[1,572,572] type:Tensor
+
+        #convert to PIL image
+        im_pil = Image.fromarray(img)
+        mask_pil = Image.fromarray(mask)
+
+        if(self.phase=='train'):
+            isTraining = 1.
+
+        else:
+            isTraining = 0.
+        img_aug, mask_aug = self.transform(im_pil,mask_pil,isTraining)
+        
+        torch.set_printoptions(profile="full")
+        #print("mask aug  is:")
+        #print(mask_aug.shape)
+        torch.set_printoptions(profile="default")
         return img_aug, mask_aug
 
     def __len__(self):
@@ -99,16 +153,10 @@ class CityDataset(Dataset):
 
 #divide data into train and val and return the dataloader depending upon train or val phase.
 def CityDataloader(df,train_img_dir,train_img_masks_dir,mean,std,phase,batch_size,num_workers):
-    if(phase=='train'):
-        df_train, df_valid = train_test_split(df, test_size=0.2, random_state=69)
-        df = df_train if phase == 'train' else df_valid
-        for_loader = CityDataset(df, train_img_dir, train_img_masks_dir, mean, std, phase)
-        dataloader = DataLoader(for_loader,batch_size=batch_size,num_workers=num_workers,pin_memory=True)
-
-    else:
-        df_valid = df
-        for_loader = CityDataset(df, train_img_dir, train_img_masks_dir, mean, std, phase)
-        dataloader = DataLoader(for_loader,batch_size=batch_size,num_workers=num_workers,pin_memory=True)
+    df_train, df_valid = train_test_split(df, test_size=0.2, random_state=69)
+    df = df_train if phase == 'train' else df_valid
+    for_loader = CityDataset(df, train_img_dir, train_img_masks_dir, mean, std, phase)
+    dataloader = DataLoader(for_loader,batch_size=batch_size,num_workers=num_workers,pin_memory=True)
 
     return dataloader
 
@@ -153,10 +201,10 @@ class Trainer(object):
         torch.set_default_tensor_type("torch.cuda.FloatTensor")
         self.net = model.to(self.device)
         cudnn.benchmark = True
-        self.criterion = torch.nn.BCEWithLogitsLoss()
+        #self.criterion = torch.nn.BCEWithLogitsLoss()
+        self.criterion = torch.nn.CrossEntropyLoss()
         self.optimizer = optim.Adam(self.net.parameters(),lr=self.lr)
-        self.scheduler = ReduceLROnPlateau(self.optimizer,mode='min',patience=3,verbose=True)
-        #print("before dataloader called")
+        self.scheduler = ReduceLROnPlateau(self.optimizer,mode='min',patience=30,verbose=True)
         self.dataloaders = {phase: CityDataloader(df, train_img_dir,train_img_masks_dir, mean, std,
                                                 phase=phase,batch_size=self.batch_size[phase],
                                                 num_workers=self.num_workers) for phase in self.phases}
@@ -167,12 +215,36 @@ class Trainer(object):
     def forward(self,inp_images,tar_mask):
         inp_images = inp_images.to(self.device)
         tar_mask = tar_mask.to(self.device)
-        inp_images = inp_images.unsqueeze(0) # adding dimension for batch (s/b 1,1,572,572)
+        #print(inp_images.shape)
+        #print(tar_mask.shape)
+
+        #inp_images = inp_images.unsqueeze(0) # adding dimension for batch (s/b 1,1,572,572)
         pred_mask = self.net(inp_images)
-        # so pred mask is tensor [1,2,388,388], target mask is [1,1,388,388]
         # target mask is the one from my preprocessing, pred is what came out of my Unet
-        loss = self.criterion(pred_mask,tar_mask)
-        return loss, pred_mask
+        #print("target mask: ")
+        #print(tar_mask)
+        
+        tar_mask=tar_mask.long()#change it to Long type
+        tar_mask = tar_mask.squeeze(1) #get ris of one dim of target mask to calculate loss
+        
+        #pred_mask=pred_mask.squeeze(0)
+        #tar_mask=tar_mask.squeeze(0)
+
+        torch.set_printoptions(profile="full")
+        #print("target mask: ")
+        #print(tar_mask)
+        #torch.set_printoptions(profile="default")
+        #print(torch.argmax(pred_mask,1))
+        #print(tar_mask)
+        #print("tar mask: ")
+        #print(torch.max(tar_mask))
+        #print("pred mask shape: ")
+        #print(pred_mask.shape)
+        torch.set_printoptions(profile="default")
+
+
+        loss = self.criterion(pred_mask,tar_mask) #changed from pred_mask to my_tensor
+        return loss, pred_mask #changed from pred_mask to my_tensor
 
     def iterate(self,epoch,phase):
         measure = Scores(phase, epoch)
@@ -194,6 +266,7 @@ class Trainer(object):
                     self.optimizer.step()
                     self.optimizer.zero_grad()
             running_loss += loss.item()
+            #pred_mask = pred_mask.detach().cuda() #cuda expected cpu
             pred_mask = pred_mask.detach().cpu()
             measure.update(mask_target,pred_mask)
         epoch_loss = (running_loss*self.accumulation_steps)/total_batches
@@ -218,7 +291,7 @@ class Trainer(object):
             if val_loss < self.best_loss:
                 print("******** New optimal weights found, saving state ********")
                 state["best_loss"] = self.best_loss = val_loss
-                torch.save(state, "./model_office_100_defaults_epoch_10xlr2_myunet_binary.pth")
+                torch.save(state, "./model_office_12seg.pth")
             print ()
 
 def main():
@@ -229,7 +302,7 @@ def main():
     # model_trainer2.start()
     
     model = UNet()
-    
+    model = model.cuda()#cuda expected but got cpu
     model_trainer = Trainer(model)
     model_trainer.start()
     
